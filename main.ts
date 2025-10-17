22fafd1fc4af2cf6bf78894900f9023c588f0079
1254312312543 <frankjrs12543@gmail.com>
2025-10-16 22:53:15 -0300

    Conversión de código de hechizos en "forever" a funciones

--- main.ts
+++ main.ts
@@ -8,6 +8,71 @@ namespace SpriteKind {
     export const Speedy_spell = SpriteKind.create()
     export const Big_spell = SpriteKind.create()
 }
+function Define_Artifacts () {
+    Array_Artifact_1 = [
+    0,
+    50,
+    50,
+    1500,
+    500,
+    5
+    ]
+    // 0:RoF
+    // 1:BVx
+    // 2:BVy
+    // 3:CD
+    // 4:IT
+    // 
+    Array_Artifact_2 = [
+    0,
+    0,
+    50,
+    3000,
+    500,
+    6
+    ]
+    Array_Artifact_3 = [
+    0,
+    0,
+    50,
+    3000,
+    1500,
+    3
+    ]
+    sprites.setDataString(Artifact_1, "Spell_1", "" + randint(4, 6))
+    sprites.setDataString(Artifact_1, "Spell_2", "" + randint(4, 6))
+    sprites.setDataString(Artifact_1, "Spell_3", "" + randint(4, 6))
+    sprites.setDataString(Artifact_1, "Spell_4", "" + randint(1, 6))
+    sprites.setDataString(Artifact_1, "Spell_5", "" + randint(1, 6))
+    sprites.setDataString(Artifact_1, "Spell_6", "" + randint(1, 6))
+    sprites.setDataString(Artifact_2, "Spell_1", "" + randint(4, 6))
+    sprites.setDataString(Artifact_2, "Spell_2", "" + randint(4, 6))
+    sprites.setDataString(Artifact_2, "Spell_3", "" + randint(4, 6))
+    sprites.setDataString(Artifact_2, "Spell_4", "" + randint(1, 6))
+    sprites.setDataString(Artifact_2, "Spell_5", "" + randint(1, 6))
+    sprites.setDataString(Artifact_2, "Spell_6", "" + randint(1, 6))
+    sprites.setDataString(Artifact_3, "Spell_1", "" + randint(4, 6))
+    sprites.setDataString(Artifact_3, "Spell_2", "" + randint(4, 6))
+    sprites.setDataString(Artifact_3, "Spell_3", "" + randint(4, 6))
+    sprites.setDataString(Artifact_3, "Spell_4", "" + randint(1, 6))
+    sprites.setDataString(Artifact_3, "Spell_5", "" + randint(1, 6))
+    sprites.setDataString(Artifact_3, "Spell_6", "" + randint(1, 6))
+}
+controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
+    music.setVolume(90)
+    music.play(music.createSong(hex`0064000408080300001c00010a006400f401640000040000000000000000000000000005000004260100000800020a0c08001000020d0f10001800020a0c18002000020c0d20002800020a0f28003000020c1230003800020a0f38004000020c1140004800020a1248005000020d1650005800020a1258006000020c1460006400020a166400680002061268006c000208146c0070000205117000740002061278007c000208118000840002060884008800020f1188008c0002080a8c009000020d0f90009400020c0d98009c0002080aa000a400020608a400a800020f11a800ac0002080aac00b000020d0fb000b400020a0cb800bc00020f11c000c400020608c400c800020f11c800cc0002080acc00d000020d0fd000d400020c0dd800dc0002080ae000e400020608e400e800020f11e800ec0002080aec00f000020d0ff000f400020c0df800fc00020f1106001c00010a006400f401640000040000000000000000000000000000000002480000000800011b08001000011d10001800011b18002000011920002800011b28003000011d30003800011b38004000011940004800011b48005000011d50005800011b58006000011909010e02026400000403780000040a000301000000640001c80000040100000000640001640000040100000000fa0004af00000401c80000040a00019600000414000501006400140005010000002c0104dc00000401fa0000040a0001c8000004140005d0076400140005d0070000c800029001f40105c201f4010a0005900114001400039001000005c201f4010500058403050032000584030000fa00049001000005c201f4010500058403c80032000584030500640005840300009001049001000005c201f4010500058403c80064000584030500c8000584030000f40105ac0d000404a00f00000a0004ac0d2003010004a00f0000280004ac0d9001010004a00f0000280002d00700040408070f0064000408070000c80003c800c8000e7d00c80019000e64000f0032000e78000000fa00032c01c8000ee100c80019000ec8000f0032000edc000000fa0003f401c8000ea901c80019000e90010f0032000ea4010000fa0001c8000004014b000000c800012c01000401c8000000c8000190010004012c010000c80002c800000404c8000f0064000496000000c80002c2010004045e010f006400042c010000640002c409000404c4096400960004f6090000f40102b80b000404b80b64002c0104f40b0000f401022003000004200300040a000420030000ea01029001000004900100040a000490010000900102d007000410d0076400960010d0070000c800d80080008100040001020b84008500040001020b88008900040001020b8c008d00040001020b90009100040001020b98009900040001020ba000a100040001020ba400a500040001020ba800a900040001020bac00ad00040001020bb000b100040001020bb800b900040001020bc000c100040001020bc400c500040001020bc800c900040001020bcc00cd00040001020bd000d100040001020bd800d900040001020be000e100040001020be400e500040001020be800e900040001020bec00ed00040001020bf000f100040001020bf800f900040001020b`), music.PlaybackMode.LoopingInBackground)
+})
+function Big_Spell (Artifact: Sprite, Artifact_array: any[]) {
+    prueba = sprites.createProjectileFromSprite(img`
+        . 2 2 2 2 . 
+        2 2 2 2 2 2 
+        2 2 2 2 2 2 
+        2 2 2 2 2 2 
+        2 2 2 2 2 2 
+        . 2 2 2 2 . 
+        `, Artifact, 0, 40)
+    prueba.setKind(SpriteKind.Big_spell)
+}
 function HUD_Inventory () {
     if (Inventory_HUD_visibility == 0) {
         Inventory_box.setFlag(SpriteFlag.Invisible, false)
@@ -22,6 +87,16 @@ function HUD_Inventory () {
         Artifact_3.setFlag(SpriteFlag.Invisible, false)
         player_.setFlag(SpriteFlag.Invisible, false)
     }
+}
+function Following_Spell (Artifact: Sprite, Artifact_array: any[]) {
+    projectile_4 = sprites.createProjectileFromSprite(img`
+        . 4 2 . 
+        4 2 4 2 
+        2 4 2 4 
+        . 2 4 . 
+        `, Artifact, 0, 0)
+    projectile_4.setKind(SpriteKind.Following_spell)
+    projectile_4.follow(player_, 20)
 }
 controller.player2.onButtonEvent(ControllerButton.Up, ControllerButtonEvent.Pressed, function () {
     Spell_casting()
@@ -46,6 +121,23 @@ sprites.onOverlap(SpriteKind.Friendly_projectile, SpriteKind.Projectile, functio
     Proyectiles_in_screen += -1
     sprites.destroy(otherSprite)
 })
+function Speedy_Spell (Artifact: Sprite, Artifact_array: number[]) {
+    if (sprites.readDataNumber(Artifact, "HP") > 0) {
+        projectile_3 = sprites.createProjectileFromSprite(img`
+            . 4 4 . 
+            4 2 2 4 
+            4 2 2 4 
+            . 4 4 . 
+            `, Artifact, 0, 0)
+        projectile_3.setKind(SpriteKind.Speedy_spell)
+        projectile_3.follow(player_, 60)
+        pause(100)
+        Artifact_array[1] = projectile_3.vx
+        Artifact_array[2] = projectile_3.vy
+        projectile_3.follow(player_, 0)
+        projectile_3.setVelocity(Artifact_array[1], Artifact_array[2])
+    }
+}
 controller.menu.onEvent(ControllerButtonEvent.Pressed, function () {
     if (Inventory_HUD_visibility == 0) {
         Inventory_HUD_visibility = 1
@@ -177,39 +269,6 @@ sprites.onOverlap(SpriteKind.Friendly_projectile, SpriteKind.Enemy, function (sp
         sprites.destroy(otherSprite)
     }
 })
-function Establecer_Artifacts () {
-    Array_Artifact_1 = [
-    0,
-    50,
-    50,
-    1500,
-    500,
-    10
-    ]
-    // 0:RoF
-    // 1:Vx
-    // 2:Vy
-    // 3:CD
-    // 4:IT
-    // 5:Repeats
-    // 6:HP
-    Array_Artifact_2 = [
-    0,
-    0,
-    50,
-    3000,
-    500,
-    5
-    ]
-    Array_Artifact_3 = [
-    0,
-    0,
-    50,
-    3000,
-    1500,
-    3
-    ]
-}
 sprites.onCreated(SpriteKind.Projectile, function (sprite) {
     timer.after(100, function () {
         if (sprite.kind() == SpriteKind.Following_spell) {
@@ -622,17 +681,17 @@ sprites.onOverlap(SpriteKind.Player_Hitbox, SpriteKind.Projectile, function (spr
     Proyectiles_in_screen += -1
     sprites.destroy(otherSprite)
 })
-let prueba: Sprite = null
-let projectile_3: Sprite = null
-let projectile_4: Sprite = null
 let Level_HP = 0
 let Equiped_artifact: number[] = []
+let Invulnerability = 0
+let Damage = 0
+let projectile_3: Sprite = null
+let player_spell: Sprite = null
+let projectile_4: Sprite = null
+let prueba: Sprite = null
 let Array_Artifact_3: number[] = []
 let Array_Artifact_2: number[] = []
 let Array_Artifact_1: number[] = []
-let Invulnerability = 0
-let Damage = 0
-let player_spell: Sprite = null
 let HP_frame: Sprite = null
 let Players_hitbox: Sprite = null
 let player_: Sprite = null
@@ -1008,7 +1067,7 @@ HP_frame.setPosition(8, 95)
 HP_frame.setFlag(SpriteFlag.RelativeToCamera, true)
 player_.setPosition(80, 150)
 Players_hitbox.setPosition(player_.x, player_.y)
-Establecer_Artifacts()
+Define_Artifacts()
 forever(function () {
     Crosshair.setPosition(player_.x - Math.cos(Direction) * 10, player_.y - Math.sin(Direction) * 10)
     if (player_.y < 120) {
@@ -1032,14 +1091,7 @@ forever(function () {
 forever(function () {
     for (let index = 0; index < Array_Artifact_3[5]; index++) {
         if (sprites.readDataNumber(Artifact_3, "HP") > 0) {
-            projectile_4 = sprites.createProjectileFromSprite(img`
-                . 4 2 . 
-                4 2 4 2 
-                2 4 2 4 
-                . 2 4 . 
-                `, Artifact_3, 0, 0)
-            projectile_4.setKind(SpriteKind.Following_spell)
-            projectile_4.follow(player_, 20)
+            Following_Spell(Artifact_3, Array_Artifact_3)
             pause(Array_Artifact_3[4])
         }
     }
@@ -1047,37 +1099,15 @@ forever(function () {
 })
 forever(function () {
     for (let index = 0; index < Array_Artifact_1[5]; index++) {
-        if (sprites.readDataNumber(Artifact_2, "HP") > 0) {
-            projectile_3 = sprites.createProjectileFromSprite(img`
-                . 4 4 . 
-                4 2 2 4 
-                4 2 2 4 
-                . 4 4 . 
-                `, Artifact_2, 0, 0)
-            projectile_3.setKind(SpriteKind.Speedy_spell)
-            projectile_3.follow(player_, 60)
-            pause(100)
-            Array_Artifact_1[1] = projectile_3.vx
-            Array_Artifact_1[2] = projectile_3.vy
-            projectile_3.follow(player_, 0)
-            projectile_3.setVelocity(Array_Artifact_1[1], Array_Artifact_1[2])
-            pause(Array_Artifact_1[4])
-        }
+        Speedy_Spell(Artifact_2, Array_Artifact_2)
+        pause(Array_Artifact_1[4])
     }
     pause(Array_Artifact_1[3])
 })
 forever(function () {
     for (let index = 0; index < Array_Artifact_2[5]; index++) {
         if (sprites.readDataNumber(Artifact_1, "HP") > 0) {
-            prueba = sprites.createProjectileFromSprite(img`
-                . 2 2 2 . 
-                2 2 2 2 2 
-                2 2 2 2 2 
-                2 2 2 2 2 
-                . 2 2 2 . 
-                `, Artifact_1, 0, 0)
-            prueba.setKind(SpriteKind.Big_spell)
-            prueba.setVelocity(Array_Artifact_2[1], Array_Artifact_2[2])
+            Big_Spell(Artifact_1, Array_Artifact_1)
             pause(Array_Artifact_2[4])
         }
     }
